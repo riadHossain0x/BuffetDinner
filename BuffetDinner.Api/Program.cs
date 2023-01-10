ac5568d83fa95d3f205737e9d7a196ea83be2a96
Riad <riadofficial.me@gmail.com>
2023-01-10 09:44:18 +0600

    UseExceptionHandler Middleware & Error Endpoint

--- BuffetDinner.Api/Program.cs
+++ BuffetDinner.Api/Program.cs
@@ -7,11 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
-    builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
+
+    //builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
+
+    builder.Services.AddControllers();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
+
     app.UseHttpsRedirection();
 
     app.MapControllers();
